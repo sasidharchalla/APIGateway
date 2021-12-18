106565e88de63324bcef9f3bddb775f4c63ebed3
Sasidhar S Challa <SasidharS.Challa@meijer.com>
2021-12-18 20:05:08 +0530

    main.tf commit

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "get_lambda_func_name" {
     provisioner "local-exec" {
       command = "aws lambda list-functions --no-verify-ssl --region ${var.region} --query 'Functions[?starts_with(FunctionName, `${var.lambda_name_query_text}`) == `true`].FunctionName' --output yaml > ${data.template_file.lambda_template.rendered}"
       //the below line needs to be removed since its a powershell command(for testing on windows), and only works on windows, But CI pipeline runs on Linux machine.
-      interpreter = ["PowerShell", "-Command"]
+      //interpreter = ["PowerShell", "-Command"]
     }
   
 }
@@ -21,7 +21,7 @@ data "local_file" "lambda_names" {
 }
 
 locals {
-  list_lambda_names = tolist(yamldecode(file("./utf8y.yaml")))
+  list_lambda_names = tolist(yamldecode(data.local_file.lambda_names.content))
 }
 
 module "apigateway" {
