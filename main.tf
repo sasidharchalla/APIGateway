36e25cf815641fc0726c4aac86ead8b52da04b7a
Sasidhar S Challa <SasidharS.Challa@meijer.com>
2021-12-18 20:25:40 +0530

    removing Powershell interpreter

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ resource "null_resource" "get_lambda_func_name" {
     }
     provisioner "local-exec" {
       command = "aws lambda list-functions --no-verify-ssl --region ${var.region} --query 'Functions[?starts_with(FunctionName, `${var.lambda_name_query_text}`) == `true`].FunctionName' --output yaml > ${data.template_file.lambda_template.rendered}"
-      //the below line needs to be removed since its a powershell command(for testing on windows), and only works on windows, But CI pipeline runs on Linux machine.
-      //interpreter = ["PowerShell", "-Command"]
     }
   
 }
@@ -21,19 +19,15 @@ data "local_file" "lambda_names" {
 }
 
 locals {
-  list_lambda_names = tolist(yamldecode(data.local_file.lambda_names.content))
+  list_lambda_names = toset(yamldecode(data.local_file.lambda_names.content))
 }
 
-/*module "apigateway" {
+module "apigateway" {
   source                    = "./modules/apigateway"
   api_name                  = var.api_name
-}*/
-
-output "lambda_names" {
- value = local.list_lambda_names[2]
 }
 
-/*module "apiintegration" {
+module "apiintegration" {
   for_each = local.list_lambda_names
   source = "./modules/api_integration"
   function_name = each.key
@@ -46,5 +40,5 @@ module "apideployment" {
   stage_name = var.stage_name
   api_gateway = module.apigateway.api
   depends_on = [module.apigateway, module.apiintegration]
-}*/
+}
 
