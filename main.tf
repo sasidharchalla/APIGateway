26724849aae4bab7a8520209241dcba7db82dc5f
Sasidhar S Challa <SasidharS.Challa@meijer.com>
2021-12-18 21:15:32 +0530

    main.tf update

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-resource "null_resource" "get_lambda_func_name" {
-    triggers = {
-        build_number = "${timestamp()}"
-    }
-    provisioner "local-exec" {
-      command = "aws lambda list-functions --no-verify-ssl --region ${var.region} --query 'Functions[?starts_with(FunctionName, `${var.lambda_name_query_text}`) == `true`].FunctionName' --output yaml > ${data.template_file.lambda_template.rendered}"
-    }
-  
-}
-
-
-data "template_file" "lambda_template" {
-    template = "${path.module}/lambda_names.yaml"
-}
-
-data "local_file" "lambda_names" {
-    filename = "${data.template_file.lambda_template.rendered}"
-    depends_on = [null_resource.get_lambda_func_name]
-}
-
-locals {
-  list_lambda_names = toset(yamldecode(data.local_file.lambda_names.content))
-}
-
 module "apigateway" {
   source                    = "./modules/apigateway"
   api_name                  = var.api_name
